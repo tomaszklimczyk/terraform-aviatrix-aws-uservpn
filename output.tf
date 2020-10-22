604ffbe13adfc934f03a4163684fd94a1d681253
Tomasz Klimczyk <tomasz.klimczyk@bigswitch.com>
2020-10-22 20:18:21 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
-output "vpc" {
-  description = "The created VPC as an object with all of it's attributes. This was created using the aviatrix_vpc resource."
-  value = aviatrix_vpc.default
+output "vnet" {
+  description = "The created net as an object with all of it's attributes. This was created using the aviatrix_vpc resource."
+  value       = aviatrix_vpc.default
 }
 
 output "spoke_gateway" {
   description = "The created Aviatrix spoke gateway as an object with all of it's attributes."
-  value = var.ha_gw ? aviatrix_spoke_gateway.ha[0] : aviatrix_spoke_gateway.single[0]
+  value       = aviatrix_spoke_gateway.default
 }
 
 output "vpn_gateway" {
